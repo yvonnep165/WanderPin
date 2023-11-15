96af5332c7812ea331b080784315f82c6d0d83ee
yaqi-lyu <iqay_lyu@hotmail.com>
2023-11-15 15:00:34 -0800

    added tab navigation

--- components/Home.js
+++ components/Home.js
@@ -1,14 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import Visited from "./Visited";
+import Wishlist from "./Wishlist";
+import { colors } from "../styles/Colors";
+
+const TopTab = createMaterialTopTabNavigator();
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
-}
+    <TopTab.Navigator screenOptions={{ tabBarStyle: styles.tabBar }}>
+      <TopTab.Screen name="Visited" component={Visited} />
+      <TopTab.Screen name="Wishlist" component={Wishlist} />
+    </TopTab.Navigator>
+  );
+};
 
-export default Home
+export default Home;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  tabBar: {
+    backgroundColor: colors.lightGreen,
+    height: 60,
+    borderRadius: 20,
+  },
+});
---
+++ components/Visited.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Visited = () => {
+  return (
+    <View>
+      <Text>Visited</Text>
+    </View>
+  )
+}
+
+export default Visited
+
+const styles = StyleSheet.create({})
