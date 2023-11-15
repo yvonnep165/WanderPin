575f8ac92f54917df1c21447bb0beaf0e987bb59
yaqi-lyu <iqay_lyu@hotmail.com>
2023-11-14 21:40:07 -0800

    initial the project and add a color palette

---
+++ styles/Colors.js
@@ -0,0 +1,9 @@
+export const colors = {
+    lightYellow: "#fcf0cf",
+    darkYellow: "#f3c853",
+    lightGreen: "#e8eedd",
+    darkGreen: "#bbcd98",
+    deepGreen: "#9ab667",
+    black: "#29292a",
+    backgroundGreen: "#f8f9f5",
+}
