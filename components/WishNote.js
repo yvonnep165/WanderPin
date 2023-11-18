2479d2e450abd37ce242db0c5819e114dc3fe7b2
yvonnep165 <pan.432@osu.edu>
2023-11-17 23:24:56 -0800

    added save and cancel buttons for wishlist and adjusted the layout of elements in the wishNote component

--- components/WishNote.js
+++ components/WishNote.js
@@ -2,6 +2,7 @@ import { View, Text, ScrollView, StyleSheet } from 'react-native';
 import React, { useState } from 'react';
 import PressableButton from "./PressableButton";
 import { Ionicons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
 import { MaterialIcons } from '@expo/vector-icons'; 
 import { colors } from "../styles/Colors";
 import { getContainerStyles } from "../components/SafeArea";
@@ -9,12 +10,22 @@ import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Switch } from '@rneui/themed';
 import InputField from './InputField';
 
-export default function WishNote() {
+export default function WishNote( { navigation } ) {
   const [reminder, setReminder] = useState(false);
 
   // safe area
   const insets = useSafeAreaInsets();
   const container = getContainerStyles(insets);
+
+  // go back to the wishlist screen
+  const handleCancel = () => {
+    navigation.goBack();
+  };
+
+  // save the data to notes collection
+  const handleSubmit = () => {
+    navigation.goBack();
+  };
 
   return (
     <View style={[styles.container, container]}>
@@ -25,26 +36,30 @@ export default function WishNote() {
       <View style={[styles.info, styles.label]}>
         <Ionicons
           name="location"
-          size={15}
-          color={colors.darkYellow}
+          size={20}
+          color={colors.deepYellow}
         />
         <Text>Location</Text>
+        <AntDesign name="right" size={14} color={colors.black} />
       </View>
       <View style={styles.info}>
         <Text style={styles.title}>Note</Text>
         <InputField placeholder="Write your important note (optional)" height={200}/>
       </View>
       <View style={[styles.info, styles.label]}>
-        <MaterialIcons name="add-location-alt" size={15} color={colors.darkYellow} />
+        <MaterialIcons name="add-location-alt" size={20} color={colors.deepYellow} />
         <Text>Add To List</Text>
+        <Text>  ? favorite</Text>
+        <AntDesign name="right" size={14} color={colors.black} />
       </View>
       <View style={[styles.info, styles.label]}>
         <Ionicons
           name="calendar"
-          size={15}
-          color={colors.darkYellow}
+          size={20}
+          color={colors.deepYellow}
         />
         <Text>Set Date Reminder</Text>
+        { /* add a switch to turn on date reminder */ }
         <Switch
           value={reminder}
           onValueChange={(value) => setReminder(value)}
@@ -52,7 +67,28 @@ export default function WishNote() {
         />
       </View>
       <View style={styles.reminder}>
-        {reminder && <Text>Date Reminder</Text>}
+      { /* date reminder notification setting will replace */ }
+      {reminder && (
+      <View style={styles.dateReminderContainer}>
+        <Text style={styles.dateReminderText}>Date Reminder</Text>
+      </View>
+      )}
+      </View>
+      {/* cancel and save button */}
+      <View style={styles.buttons}>
+        <PressableButton
+          pressedStyle={styles.pressed}
+          onPressFunction={handleCancel}
+        >
+          <Text style={styles.cancelText}>Cancel</Text>
+        </PressableButton>
+        <PressableButton
+          defaultStyle={styles.submit}
+          pressedStyle={styles.pressed}
+          onPressFunction={handleSubmit}
+        >
+          <Text style={styles.submitText}>Add to Wishlist</Text>
+        </PressableButton>
       </View>
     </View>
   )
@@ -63,6 +99,7 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingHorizontal: 25,
     padding: 30,
+    marginTop: 30,
   },
   title: {
     fontWeight: 'bold',
@@ -72,6 +109,7 @@ const styles = StyleSheet.create({
   },
   info: {
     marginBottom: 10, 
+    marginTop: 5,
     marginLeft: 20,
     marginRight: 20,
   },
@@ -81,5 +119,41 @@ const styles = StyleSheet.create({
   },
   reminder: {
     alignItems: "center",
-  }
+  },
+  buttons: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+    justifyContent: "center",
+  },
+  pressed: {
+    opacity: 0.7,
+  },
+  cancelText: {
+    color: colors.deepYellow,
+    fontWeight: "bold",
+  },
+  submitText: {
+    color: colors.white,
+    fontWeight: "bold",
+  },
+  submit: {
+    backgroundColor: colors.deepYellow,
+    width: "75%",
+    height: 40,
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  dateReminderContainer: {
+    backgroundColor: colors.deepGreen,
+    borderRadius: 20,
+    padding: 10,
+    marginBottom: 10,
+    width: "90%",
+  },
+  dateReminderText: {
+    color: colors.white,
+    fontWeight: "bold",
+  },
 });
