9d1ec8beb3b18b5ec2c59b7a7c3e44a5aa2868a4
yvonnep165 <pan.432@osu.edu>
2023-11-17 22:22:23 -0800

    added a switch for date reminder setting

--- components/WishNote.js
+++ components/WishNote.js
@@ -1,14 +1,17 @@
 import { View, Text, ScrollView, StyleSheet } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import PressableButton from "./PressableButton";
 import { Ionicons } from "@expo/vector-icons";
 import { MaterialIcons } from '@expo/vector-icons'; 
 import { colors } from "../styles/Colors";
 import { getContainerStyles } from "../components/SafeArea";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Switch } from '@rneui/themed';
 import InputField from './InputField';
 
 export default function WishNote() {
+  const [reminder, setReminder] = useState(false);
+
   // safe area
   const insets = useSafeAreaInsets();
   const container = getContainerStyles(insets);
@@ -19,26 +22,37 @@ export default function WishNote() {
         <Text style={styles.title}>Title</Text>
         <InputField placeholder="Write the title"/>
       </View>
-      <View style={styles.info}>
+      <View style={[styles.info, styles.label]}>
         <Ionicons
           name="location"
           size={15}
           color={colors.darkYellow}
         />
+        <Text>Location</Text>
       </View>
       <View style={styles.info}>
         <Text style={styles.title}>Note</Text>
         <InputField placeholder="Write your important note (optional)" height={200}/>
       </View>
-      <View style={styles.info}>
+      <View style={[styles.info, styles.label]}>
         <MaterialIcons name="add-location-alt" size={15} color={colors.darkYellow} />
+        <Text>Add To List</Text>
       </View>
-      <View style={styles.info}>
+      <View style={[styles.info, styles.label]}>
         <Ionicons
           name="calendar"
           size={15}
           color={colors.darkYellow}
         />
+        <Text>Set Date Reminder</Text>
+        <Switch
+          value={reminder}
+          onValueChange={(value) => setReminder(value)}
+          color={colors.deepGreen}
+        />
+      </View>
+      <View style={styles.reminder}>
+        {reminder && <Text>Date Reminder</Text>}
       </View>
     </View>
   )
@@ -61,4 +75,11 @@ const styles = StyleSheet.create({
     marginLeft: 20,
     marginRight: 20,
   },
+  label: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  reminder: {
+    alignItems: "center",
+  }
 });
