fd3b3fe9d7ebd354438366905cf0d4c97e3055ea
yvonnep165 <pan.432@osu.edu>
2023-11-18 20:14:17 -0800

    added firestoreHelper function

--- firebase/firestoreHelper.js
+++ firebase/firestoreHelper.js
@@ -1,4 +1,4 @@
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, doc, deleteDoc, updateDoc } from "firebase/firestore";
 import { database } from "./firebaseSetup";
 
 export async function writeJournalToDB(journal) {
@@ -9,5 +9,73 @@ export async function writeJournalToDB(journal) {
     console.log("Document written with ID: ", docRef.id);
   } catch (err) {
     console.log(err);
+  }
+}
+
+// add a new list to the lists collection
+export async function writeListToDB(list) {
+  try{
+      const docRef = await addDoc(collection(database, "lists"), list); 
+  } catch (err){
+      console.log(err);
+  }
+}
+
+// update a list's information
+export async function updateList(id, title, color, icon) {
+  try {
+      const updateList = doc(database, "lists", id);
+      await updateDoc(updateList,  {
+          title: title, 
+          color: color,
+          icon: icon,
+      });
+  } catch(err) {
+      console.log(err);
+  }
+}
+
+// delete a list from database
+export async function deleteListFromDB(id) {
+  try{
+      await deleteDoc(doc(database, "lists", id))
+  }
+  catch(err) {
+      console.log(err)
+  }
+}
+
+// add a new note to the notes collection
+export async function writeNoteToDB(note) {
+  try{
+      const docRef = await addDoc(collection(database, "notes"), note); 
+  } catch (err){
+      console.log(err);
+  }
+}
+
+// update a note's information
+export async function updateNote(id, title, location, note, listID, reminder) {
+  try {
+      const updateNote = doc(database, "notes", id);
+      await updateDoc(updateNote,  {
+          title: title, 
+          location: location,
+          note: note,
+          listId: listID,
+          reminder: reminder,
+      });
+  } catch(err) {
+      console.log(err);
+  }
+}
+
+// delete a note from database
+export async function deleteNoteFromDB(id) {
+  try{
+      await deleteDoc(doc(database, "notes", id))
+  }
+  catch(err) {
+      console.log(err)
   }
 }
