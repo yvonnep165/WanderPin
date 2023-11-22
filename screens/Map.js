ab3a368cb53d9fdf5210cb3325a24d612f99eb7e
yvonnep165 <pan.432@osu.edu>
2023-11-21 20:11:58 -0800

    edited to show the user's current location when enter map screen

--- screens/Map.js
+++ screens/Map.js
@@ -7,6 +7,8 @@ import PressableButton from "../components/PressableButton";
 import MapView, { Callout, Marker } from "react-native-maps";
 import * as Location from "expo-location";
 import { MAPS_API_KEY } from "@env";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { colors } from "../styles/Colors";
 
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
@@ -19,7 +21,11 @@ const Map = ( {navigation} ) => {
   const [userLocation, setUserLocation] = useState(null);
 
   useEffect(() => {
-    getUserLocation()
+    const updateLocationInterval = setInterval(() => {
+      getUserLocation();
+    }, 5000); // Update every 5 seconds, adjust as needed
+  
+    return () => clearInterval(updateLocationInterval);
   }, []);
 
   // send the selected location to other screens to perform other actions
@@ -34,6 +40,7 @@ const Map = ( {navigation} ) => {
       return true;
     }
     const response = await requestPermission();
+    console.log(response)
     return response && response.granted;
   };
 
@@ -41,8 +48,10 @@ const Map = ( {navigation} ) => {
   async function getUserLocation() {
     try {
       const hasPermission = await verifyPermission();
+      console.log("Has Permission:", hasPermission);
       if (!hasPermission) {
         Alert.alert("You need to give access to the location so that your markers around you can be shown");
+        return
       }
       const locationObject = await Location.getCurrentPositionAsync();
 
@@ -57,11 +66,35 @@ const Map = ( {navigation} ) => {
 
   return (
     <View style={[container, commonStyles.container]}>
+      {/* <GooglePlacesAutocomplete
+				placeholder="Search"
+				// fetchDetails={true}
+				// GooglePlacesSearchQuery={{
+				// 	rankby: "distance"
+				// }}
+				onPress={(data, details = null) => {
+					console.log(data, details)
+          if (details) {
+					setSelectedLocation({
+						latitude: details.geometry.location.lat,
+						longitude: details.geometry.location.lng,
+					})}
+				}}
+				query={{
+					key: MAPS_API_KEY,
+					language: 'en',
+					location: `${selectedLocation?.latitude ?? ''}, ${selectedLocation?.longitude ?? ''}`
+				}}
+				styles={{
+					container: { flex: 0, position: "absolute", width: "100%", zIndex: 1 },
+					listView: { backgroundColor: "white" }
+				}}
+			/> */}
       {/* show the user's current location and the location in the selected lists*/}
       {/* select a location by clicking on map */}
       <MapView
         style={styles.map}
-        initialRegion={{
+        region={{
           latitude: userLocation? userLocation.latitude : 49.2827,
           longitude: userLocation? userLocation.longitude : -123.1207,
           latitudeDelta: 0.0922,
@@ -75,24 +108,30 @@ const Map = ( {navigation} ) => {
         }}
         provider="google"
       >
-        <Marker 
-          coordinate={userLocation}
-          draggable={true}
-        >
-          <Callout>
-            <Text>Current Location</Text>
-          </Callout>
-        </Marker>
+        { (userLocation && !selectedLocation) && 
+          (<Marker 
+            coordinate={userLocation}
+            draggable={true}
+          >
+            <Callout>
+              <Text>Current Location</Text>
+            </Callout>
+          </Marker>)
+        }
         <Marker coordinate={selectedLocation} >
           <Callout>
             <Text>selected</Text>
           </Callout>
         </Marker>
       </MapView>
-      <PressableButton
-        disabled={!selectedLocation}
-        onPressFunction={confirmLocationHandler}
-      />
+        <View style={styles.buttonContainer}>
+          <PressableButton
+            defaultStyle={styles.submit}
+            pressedStyle={styles.pressed}
+            disabled={!selectedLocation}
+            onPressFunction={confirmLocationHandler}
+          />
+        </View>
     </View>
   );
 };
@@ -103,5 +142,24 @@ const styles = StyleSheet.create({
   map: {
     width: windowWidth,
     height: windowHeight,
+  },
+  buttonContainer: {
+    position: "absolute",
+    bottom: 20, 
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  submit: {
+    backgroundColor: colors.deepYellow,
+    width: "45%",
+    height: 40,
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  pressed: {
+    opacity: 0.7,
   },
 });
