fa3039971b3e5aed590d0903d5c171f0c9188146
yaqi-lyu <iqay_lyu@hotmail.com>
2023-12-13 20:09:51 -0800

    fixed the map displaying bug on android

--- components/CustomMarker.js
+++ components/CustomMarker.js
@@ -1,31 +1,49 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React, { useState } from 'react'
+import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
 import { Callout, Marker } from "react-native-maps";
 import { colors } from "../styles/Colors";
-import { iconStyle } from '../styles/CommonStyles';
+import { iconStyle } from "../styles/CommonStyles";
 
-export default function CustomMarker({coordinate, draggable = false, message, icon }) {
-    const [calloutVisible, setCalloutVisible] = useState(false);
+export default function CustomMarker({
+  coordinate,
+  draggable = false,
+  message,
+  icon,
+}) {
+  const [calloutVisible, setCalloutVisible] = useState(false);
+  console.log(coordinate);
 
   return (
-    <Marker coordinate={coordinate} 
-        draggable={draggable}
-        onPress={() => setCalloutVisible(calloutVisible => !calloutVisible)}
+    <Marker
+      coordinate={{
+        longitude:
+          coordinate && coordinate.longitude ? coordinate.longitude : 0,
+        latitude: coordinate && coordinate.latitude ? coordinate.latitude : 0,
+      }}
+      draggable={draggable}
+      onPress={() => setCalloutVisible((calloutVisible) => !calloutVisible)}
+    >
+      {icon && (
+        <View
+          style={[
+            styles.icon,
+            { backgroundColor: colors.colorOption[icon.iconColor] },
+          ]}
         >
-        {icon && 
-        (<View style={[styles.icon, { backgroundColor: colors.colorOption[icon.iconColor] }]}>
-            {icon.iconLable}
-        </View>)}
-        {calloutVisible && 
-        (<Callout style={{ width: 200 }}>
-            <View>
-                <Text>{message}</Text>
-            </View>
-        </Callout>)}
+          {icon.iconLable}
+        </View>
+      )}
+      {calloutVisible && (
+        <Callout style={{ width: 200 }}>
+          <View>
+            <Text>{message}</Text>
+          </View>
+        </Callout>
+      )}
     </Marker>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    icon: iconStyle,
+  icon: iconStyle,
 });
